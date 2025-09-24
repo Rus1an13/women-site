5294e6df5e26b9631f588810832afdce631e071f
Rus1an13 <ruslan-1978@bk.ru>
2025-09-24 23:38:40 +0500

    women site lesson 22

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseServerError, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.template.defaultfilters import slugify
 from django.template.loader import render_to_string
 from django.urls import reverse
+
+from women.models import Women
 
 menu = [
     {'title': "О сайте", 'url_name': 'about'},
@@ -22,22 +24,36 @@ data_db = [
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
 ]
 
+cats_db = [
+    {'id':1, 'name': 'Актрисы'},
+    {'id':2, 'name': 'Певицы'},
+    {'id':3, 'name': 'Спортсменки'},
+]
+
 def index(request):
-    # t = render_to_string('women/index.html')
-    # return HttpResponse(t)
+    posts = Women.objects.filter(is_published=1)
+
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': data_db,
+        'posts': posts,
+        'cat_selected': 0,
     }
-
     return render(request, 'women/index.html', context=data)
 
 def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+    return render(request, 'women/post.html', data)
 
 def addpage(request):
     return HttpResponse("Добавление статьи")
@@ -47,6 +63,15 @@ def contact(request):
 
 def login(request):
     return HttpResponse("Авторизация")
+
+def show_category(request, cat_id):
+    data = {
+        'title': 'Отображение по рубрикам',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=data)
 
 # def page_not_found(request, exeption):
 #     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
