7074860ee21953a6e74069ef40ff9893783935d4
Rus1an13 <ruslan-1978@bk.ru>
2025-10-02 23:06:16 +0500

    women site lesson 48

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -1,10 +1,26 @@
 from django import forms
-from .models import Category, Husband
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from prompt_toolkit.validation import ValidationError
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+from .models import Category, Husband, Women
+
+
+class AddPostForm(forms.ModelForm):
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категории")
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), empty_label="Не замужем", required=False, label="Муж")
+
+    class Meta:
+        model = Women
+        fields = ['title', 'slug', 'content', 'is_published', 'cat', 'husband', 'tags']
+        widgets = {'title': forms.TextInput(attrs={'class': 'form-input'}), 'content': forms.Textarea(attrs={'cols': 50, 'rows': 5}),}
+        labels = {'slug': 'URL'}
+
+        def clean_title(self):
+            title = self.cleaned_data['title']
+            if len(title) > 50:
+                raise ValidationError("Длина превышает 50 символов")
+
+            return title
+
+class UploadFileForm(forms.Form):
+    file = forms.ImageField(label="Файл")
