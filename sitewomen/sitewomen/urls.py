ecd58bbf20da0ef36c4cb7b594d68bf34c56883b
Rus1an13 <ruslan-1978@bk.ru>
2025-10-07 23:22:24 +0500

    women site lesson 50

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+import settings
 from women import views
 
 urlpatterns = [
@@ -7,6 +10,9 @@ urlpatterns = [
     path('', include('women.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # handler404 = views.page_not_found
 
